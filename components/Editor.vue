114335bfebc06b303aa3db85d5239f69b1aba61d
Harper Reed <harper@nata2.org>
2021-09-04 18:21:31 +0000

    more swipe events

--- components/Editor.vue
+++ components/Editor.vue
@@ -7,25 +7,53 @@
     {{ content }}
   </div> -->
   <textarea  
-    v-model="content" 
+    v-model.lazy="content" 
+    placeholder="Type here"
     class="text-7xl h-full w-full p-6 font-bold" 
-    @input="onInput" 
-    v-touch:swipe="swipeHandler"/> 
+    v-touch:swipe.right="swipeRightHandler"
+    v-touch:swipe.left="swipeLeftHandler"
+    v-touch:swipe.top="swipeTopHandler"
+    v-touch:swipe.bottom="swipeBottomHandler"
+    v-touch:longtap="longtapHandler"
+    ref="editor"
+    /> 
 
 </template>
 
 <script>
 export default {
   data() {
-    return { content: "type here" };
+    return { content: "" };
+  },
+  watch : {
+    content : function(val){
+      // this.$emit('input', val);
+      console.log(val)
+    }
+  },
+  mounted() {
+    this.focusInput()
   },
   methods: {
-    onInput(e) {
-      console.log(e.target.innerText);
+    swipeLeftHandler(e) {
+      this.content = "Left Swipe";
     },
-    swipeHandler(e) {
-      this.content = "fuuuuusss";
+    swipeRightHandler(e) {
+      this.content = "Right Swipe";
     },
+    swipeTopHandler(e) {
+      this.content = "Top Swipe";
+    },
+    swipeBottomHandler(e) {
+      this.content = "Bottom Swipe";
+    },
+    longtapHandler(e) {
+      this.content = "Long Hold";
+    },
+
+    focusInput() {
+      this.$refs.editor.focus();
+    }
   },
 };
 </script>
