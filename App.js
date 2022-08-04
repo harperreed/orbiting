68995c6157e658ad609633aff224b3d2a2a054cb
Harper Reed <harper@nata2.org>
2022-08-04 11:35:01 -0500

    dark mode

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState }  from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, useColorScheme } from 'react-native';
 
 export default function App() {
+
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+  
+
   const [text, setText] = useState('');
   return (
-    <View style={containerStyle.container}>
-       <TextInput
-        style={editStyle.container}
+    <View style={[styles.container, themeContainerStyle]}>
+      <TextInput
+        style={[styles.editor, themeTextStyle]}
         placeholder="type here"
         onChangeText={newText => setText(newText)}
         defaultValue={text}
         multiline={true}
+        // autoFocus={true}
+        adjustsFontSizeToFit={true}
+        // clearTextOnFocus={true}
+        numberOfLines={4}
 
       />
       <StatusBar style="auto" />
@@ -19,18 +31,25 @@ export default function App() {
   );
 }
 
-const containerStyle = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
-
-const editStyle = StyleSheet.create({
-  container: {
+  lightContainer: {
     backgroundColor: '#fff',
+  },
+  darkContainer: {
+    backgroundColor: '#000',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#fff',
+  },
+  editor: {
     alignItems: 'center',
     justifyContent: 'center',
     padding: 10,
@@ -38,5 +57,6 @@ const editStyle = StyleSheet.create({
     width: '100%',
     fontSize: 100,
     fontWeight: 'bold',
-  },
+  }
 });
+
