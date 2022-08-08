c299a53d9c907f9373be261024fa0fb5db41946e
Harper Reed <harper@nata2.org>
2022-08-07 20:50:23 -0500

    added some locales. but disabled them until i understand way more

---
+++ locales/en.js
@@ -0,0 +1,11 @@
+const en = {  
+  EditorTitle: 'Orbiting Editor',
+  About: 'Orbiting About',
+  Settings: 'Orbiting Settings',
+  History: 'Orbiting History',
+  Help: 'Orbiting Help',
+};
+
+
+
+export default en;
---
+++ localizations.js
@@ -0,0 +1,19 @@
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+// import es from './locales/es';  
+import en from './locales/en';
+
+
+i18n.translations = {
+  en: en
+};
+
+// Set the locale once at the beginning of your app.
+i18n.locale = Localization.locale;
+
+// When a value is missing from a language it'll fallback to another language with the key present.
+i18n.fallbacks = true;
+
+
+export default i18n; 
