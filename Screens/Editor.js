d59a82a4feb9b587c0942048093e8eb0cc5452b1
Harper Reed <harper@nata2.org>
2023-05-24 23:54:11 -0500

    updated Screens/Editor.js with changes from our friend chatgpt

--- Screens/Editor.js
+++ Screens/Editor.js
@@ -1,76 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, View, Button, useColorScheme, Platform } from 'react-native';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
+import { SafeAreaView, StyleSheet, TextInput, View, Button, useColorScheme, Platform, Dimensions } from 'react-native';
 
 import { useSwipe } from '../hooks/useSwipe'
 import { StateContext } from '../StateContext';
+
 function Editor({ navigation }) {
-
   const colorScheme = useColorScheme();
+  const { message, setMessage, messageHistory, setMessageHistory } = useContext(StateContext);
+  const [fontSize, setFontSize] = useState(100);
+  const [targetFontSize, setTargetFontSize] = useState(100);
 
-  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themFabStyle = colorScheme === 'light' ? styles.lightThemeFab : styles.darkThemeFab;
-  const themeContainerStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+  const themeStyles = {
+    text: colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText,
+    fab: colorScheme === 'light' ? styles.lightThemeFab : styles.darkThemeFab,
+    container: colorScheme === 'light' ? styles.lightContainer : styles.darkContainer,
+  };
+  
   const fabVisible = Platform.OS === 'web' ? true : false;
 
-  const { message, setMessage } = React.useContext(StateContext);
-  const { messageHistory, setMessageHistory } = React.useContext(StateContext);
+  const { onTouchStart, onTouchEnd } = useSwipe(onSwipeLeft, onSwipeRight, 6);
 
-  const { onTouchStart, onTouchEnd } = useSwipe(onSwipeLeft, onSwipeRight, 6)
-
-  function addToHistory(message) {
-    const history = messageHistory;
-    const messageObject = {
-      message: message,
+  const addToHistory = useCallback((message) => {
+    setMessageHistory(prevHistory => [...prevHistory, {
+      message,
       date: new Date(),
-      id: history.length + 1
-    }
-    history.push(messageObject);
-    setMessageHistory(history);
+      id: prevHistory.length + 1,
+    }]);
+  }, [setMessageHistory]);
 
-  }
-
-  function onSwipeLeft() {
+  const onSwipeLeft = useCallback(() => {
     addToHistory(message);
-    setMessage('')
-    
-  }
-  function onSwipeRight() {
+    setMessage('');
+  }, [message, addToHistory, setMessage]);
+
+  const onSwipeRight = useCallback(() => {
     console.log("swipe right");
     navigation.navigate('Utility');
+  }, [navigation]);
+
+// Assume these are the dimensions of your text input area and your smallest font size
+const textInputWidth = Dimensions.get('window').width;
+const textInputHeight = Dimensions.get('window').height;
+const smallestFontSize = 50;  // adjust based on your smallest font size
+
+// Assume these are the dimensions of a character at the smallest font size
+const charWidth = smallestFontSize / 2;  // adjust based on your font
+const charHeight = smallestFontSize;  // adjust based on your font
+
+// Calculate the maximum number of characters that can fit in the text area
+const charsPerLine = Math.floor(textInputWidth / charWidth);
+const lines = Math.floor(textInputHeight / charHeight);
+const maxChars = charsPerLine * lines;
+
+useEffect(() => {
+  if (message.length > maxChars) {
+    setFontSize(smallestFontSize);
+  } else {
+    setFontSize(Math.max(smallestFontSize, Math.min(250, Dimensions.get('window').width / Math.sqrt(message.length))));
   }
+}, [message]);
+  
 
   return (
-    <SafeAreaView style={[styles.container, themeContainerStyle]} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd} bounces={false} alwaysBounceVertical={false}>
+    <SafeAreaView style={[styles.container, themeStyles.container]} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd} bounces={false} alwaysBounceVertical={false}>
       <TextInput
-        style={[styles.editor, themeTextStyle]}
+        style={[styles.editor, themeStyles.text, { fontSize }]}
         placeholder="type here"
         placeholderTextColor="gray"
-
+        scrollEnabled={false}
         defaultValue={message}
-
+        textBreakStrategy='simple'
         autoComplete="off"
         autoCorrect={false}
         autoCapitalize="none"
         spellCheck={false}
         textContentType="none"
-
         multiline={true}
         numberOfLines={4}
         adjustsFontSizeToFit={true}
-
-        // autoFocus={true}
-        // clearTextOnFocus={true}
-
-        onChangeText={newMessage => setMessage(newMessage)}
+        onChangeText={setMessage}
       />
       <StatusBar style="auto" />
       <Button
-
-        label='🎉'
-        style={[styles.fab, themFabStyle]}
+        title='🎉'
+        style={[styles.fab, themeStyles.fab]}
         onPress={() => navigation.navigate('Utility')}
-        color={themeTextStyle.color}
+        color={themeStyles.text.color}
         visible={fabVisible}
       >
         Menu
