d79f172fdca0dab3fdc93fe8fce70fa3ac7e658f
Harper Reed <harper@nata2.org>
2024-02-15 14:35:02 -0600

    tweaked the history

--- Screens/History.js
+++ Screens/History.js
@@ -1,37 +1,115 @@
-
-
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Avatar, DataTable, Headline, Paragraph, Subheading, ToggleButton, RadioButton, Button, Text, Switch, Title, Divider } from 'react-native-paper';
-import { StateContext } from '../StateContext';
-
-
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import {
+    Avatar,
+    DataTable,
+    Headline,
+    Paragraph,
+    Subheading,
+    ToggleButton,
+    RadioButton,
+    Button,
+    Text,
+    Switch,
+    Title,
+    Divider,
+} from "react-native-paper";
+import { StateContext } from "../StateContext";
 
 function History() {
+    const { message, setMessage } = React.useContext(StateContext);
+    const { messageHistory, setMessageHistory } =
+        React.useContext(StateContext);
 
-  const { message, setMessage } = React.useContext(StateContext);
-  const { messageHistory, setMessageHistory } = React.useContext(StateContext);
-
-  return (
-    <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'flex-start', marginLeft: 10 }}>
-      <Headline>History Screen</Headline>
-      {/* <Paragraph>{messageHistory}</Paragraph> */}
-      <Text>{message}</Text>
-      <DataTable>
-      <DataTable.Header>
-        <DataTable.Title>Message</DataTable.Title>
-
-      </DataTable.Header>
-      {messageHistory.map(r => (
-      <DataTable.Row>
-       <DataTable.Cell numeric>{r.message}</DataTable.Cell>
-
-
-     </DataTable.Row>
-      ))}
-    </DataTable>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                alignItems: "stretch",
+                justifyContent: "flex-start",
+                marginLeft: 10,
+            }}
+        >
+            {messageHistory.some((r) => r.isFavorite) && (
+                <>
+                    <Headline>Favorites</Headline>
+                    <DataTable>
+                        {messageHistory
+                            .filter((r) => r.isFavorite)
+                            .map((r, index) => (
+                                <DataTable.Row key={index}>
+                                    <DataTable.Cell style={{ flex: 3 }}>
+                                        {r.message}
+                                    </DataTable.Cell>
+                                    <DataTable.Cell numeric>
+                                        <ToggleButton
+                                            icon="star"
+                                            value="favorite"
+                                            status="checked"
+                                            onPress={() => {
+                                                const newMessageHistory = [
+                                                    ...messageHistory,
+                                                ];
+                                                newMessageHistory[
+                                                    index
+                                                ].isFavorite =
+                                                    !newMessageHistory[index]
+                                                        .isFavorite;
+                                                setMessageHistory(
+                                                    newMessageHistory,
+                                                );
+                                            }}
+                                        />
+                                    </DataTable.Cell>
+                                </DataTable.Row>
+                            ))}
+                    </DataTable>
+                </>
+            )}
+            {messageHistory.length > 0 && (
+                <>
+                    <Headline>History</Headline>
+                    <DataTable>
+                        {messageHistory.map((r, index) => (
+                            <DataTable.Row key={index}>
+                                <DataTable.Cell style={{ flex: 3 }}>
+                                    {r.message}
+                                </DataTable.Cell>
+                                <DataTable.Cell numeric>
+                                    <ToggleButton
+                                        icon={
+                                            r.isFavorite
+                                                ? "star"
+                                                : "star-outline"
+                                        }
+                                        value="favorite"
+                                        status={
+                                            r.isFavorite
+                                                ? "checked"
+                                                : "unchecked"
+                                        }
+                                        onPress={() => {
+                                            const newMessageHistory = [
+                                                ...messageHistory,
+                                            ];
+                                            newMessageHistory[
+                                                index
+                                            ].isFavorite =
+                                                !newMessageHistory[index]
+                                                    .isFavorite;
+                                            setMessageHistory(
+                                                newMessageHistory,
+                                            );
+                                        }}
+                                    />
+                                </DataTable.Cell>
+                            </DataTable.Row>
+                        ))}
+                    </DataTable>
+                </>
+            )}
+        </View>
+    );
 }
 
-export default History
+export default History;
